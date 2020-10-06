5bdae68230750ae506a35608c5a31020c0015fe5
Reef Liew <deyyingliew@gmail.com>
2020-10-06 13:05:55 -0400

    Initial Commit: Player movement and trigger the letter boxes with tags

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{   
+    Rigidbody2D myRigidbody;
+    float inputHorizontal;
+    float inputVertical;
+    public float MoveSpeed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputHorizontal = Input.GetAxis("Horizontal");
+        inputVertical = Input.GetAxis("Vertical");
+    }
+    void FixedUpdate(){
+        myRigidbody.velocity = new Vector2 ( inputHorizontal * MoveSpeed, inputVertical * MoveSpeed);
+    }
+}
