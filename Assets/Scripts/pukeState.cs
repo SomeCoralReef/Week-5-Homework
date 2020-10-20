8c35164408b783e5b772140e99abb797aa7ed2f5
Reef Liew <deyingliew@gmail.com>
2020-10-20 13:38:54 -0400

    moved the L and fixed the frame bug for puke

--- Assets/Scripts/pukeState.cs
+++ Assets/Scripts/pukeState.cs
@@ -7,11 +7,17 @@ public class pukeState : MonoBehaviour
 public List<string> puke = new List<string> {};
 public GameObject alc_8;
 public bool pukeSound = false;
+float numberToInstantiate = 1;
 
 void Update (){
     if(puke.Contains("P") && puke.Contains("U") && puke.Contains("K") && puke.Contains("E")){
+        if(numberToInstantiate > 0 ){
         Instantiate(alc_8);
         pukeSound = true;
+        numberToInstantiate --;
+        }
+
+
     }
 }
 
